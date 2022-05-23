39c7aeef0b60046bbd5c556319143a67fd4cc6cd
Nikita <eternalxbless@gmail.com>
2022-05-23 21:36:20 +0300

    beta quote generator version

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+
+
+const generate = () => {
+    let quotes = {
+        "Наполеон Хилл, журналист и писатель ":"Что разум человека может постигнуть и во что он может поверить, того он способен достичь",
+        "Альберт Эйнштейн" : "Стремитесь не к успеху, а к ценностям, которые он дает",
+    
+        "Флоренс Найтингейл":"Своим успехом я обязана тому, что никогда не оправдывалась и не принимала оправданий от других." 
+    }
+    
+    let quotesAutors = Object.keys(quotes);
+    
+    let quoteAutor = quotesAutors[Math.floor(Math.random() * quotesAutors.length)];
+    
+    let quote = quotes[quoteAutor];
+    
+    document.querySelector("#quote").textContent = quote;
+    document.querySelector("#quoteAuthor").textContent = quoteAutor;
+}
+
+
+document.querySelector("#generateQuotes").addEventListener("click", () => {
+
+    generate();
+})
